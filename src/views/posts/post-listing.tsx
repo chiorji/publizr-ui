8eabee2c3a392dfb8388cdc4e8580cbe5ea64952
Chigbogu Orji <brightorji60@gmail.com>
2025-02-07 23:29:48 +0100

    feat: screen for all post listing

--- src/views/posts/post-listing.tsx
+++ src/views/posts/post-listing.tsx
@@ -1,10 +1,10 @@
-import PostCard from './post-card';
+import { Link } from 'react-router-dom';
 import { useGetAllPostsQuery } from '../../app/api/post-slice';
 import { useMemo } from 'react';
 import { useRandomImage } from '../../hooks/use-image';
 
 const BlogListing = () => {
-  const { data } = useGetAllPostsQuery();
+  const { data, isLoading } = useGetAllPostsQuery();
 
   const posts = useMemo(() => {
     if (!data) return [];
@@ -14,13 +14,25 @@ const BlogListing = () => {
     }));
   }, [data]);
 
+  if (isLoading) return <div>Loading</div>;
+  if (!isLoading && posts.length === 0) return <div>No posts</div>;
+
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <ul className="space-y-4 text-gray-600">
         {posts?.filter(post => !post.featured).map(post => (
-          <PostCard key={post.post_id} post={post} />
+          <li key={post.post_id} className="p-4 border border-gray-200 rounded-lg">
+            <Link to={`/posts/${post.post_id}`} className="flex items-center space-x-4">
+              <img src={post.poster_card} alt={post.title} className="w-16 h-16 object-cover rounded-sm" />
+              <div>
+                <span className="text-lg font-bold">{post.title}</span>
+                <p className="text-sm text-gray-500">By {post.username}</p>
+                <p className="text-sm">{post.excerpt}</p>
+              </div>
+            </Link>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
