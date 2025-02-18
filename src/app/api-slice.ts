9c15fcd81ad79050e6b85fce67e386f0eedce684
Chigbogu Orji <brightorji60@gmail.com>
2025-02-18 18:04:31 +0100

    chore: add authorization token to request header

--- src/app/api-slice.ts
+++ src/app/api-slice.ts
@@ -1,12 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RootState } from './store';
 const baseUrl = 'http://localhost:8080'
 
 export const apiSlice = createApi({
   reducerPath: 'apiSlice',
   baseQuery: fetchBaseQuery({
     baseUrl: `${baseUrl}/`,
-    prepareHeaders: (headers) => {
-      headers.set('Authorization', `Bearer `);
+    prepareHeaders: (headers, { getState }) => {
+      const state = getState() as RootState;
+      if (state.users.token) {
+        headers.set('Authorization', `Bearer ${state.users.token}`);
+      }
       headers.set('Content-Type', 'application/json');
       return headers;
     },
