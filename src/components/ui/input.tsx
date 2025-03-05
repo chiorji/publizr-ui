124ecaa10e4b0ecfbaa3ef9268b6408eb37e28d6
Chigbogu Orji <brightorji60@gmail.com>
2025-03-05 10:26:34 +0100

    feat: updating post

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,113 @@
+import React, { ComponentProps, ChangeEventHandler, ReactNode } from "react";
+import { Alert, AlertDescription } from "./alert";
+import { AlertCircle } from "lucide-react";
+import { OptionContract } from "../../types";
+
+interface TextInputProps extends ComponentProps<'input'> {
+  label: string
+  error?: string
+  suffix?: ReactNode
+}
+
+interface TextAreaProps extends ComponentProps<'textarea'> {
+  label: string
+  error?: string
+};
+
+interface RadioInputProps extends TextInputProps {
+  active: boolean
+}
+
+interface SelectFieldProps extends ComponentProps<'select'> {
+  options: OptionContract[]
+  selectedValue: string | number
+  label: string
+  onChange: ChangeEventHandler<HTMLSelectElement>
+  error?: string
+}
+
+export const TextInput: React.FC<TextInputProps> = ({ error, label, suffix, ...rest }) => {
+  return (
+    <div className="space-y-2 relative">
+      <label htmlFor={rest.name} className="block text-sm font-medium text-gray-700">{label}</label>
+      <input
+        type="text"
+        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        {...rest}
+      />
+      {suffix && <div className="absolute rounded-sm top-[30px] right-[2px]">{suffix}</div>}
+      {error && (
+        <Alert variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
+    </div>
+  )
+}
+
+export const TextAreaInput: React.FC<TextAreaProps> = (props) => {
+  const { error, label, ...rest } = props;
+  return (
+    <div className="space-y-2">
+      <label htmlFor={rest.name} className="block text-sm font-medium text-gray-700">
+        {label}
+      </label>
+      <textarea
+        rows={10}
+        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        {...rest}
+      />
+      {error && (
+        <Alert variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
+    </div>
+  )
+}
+
+export const RadioInput: React.FC<RadioInputProps> = (props) => {
+  const { error, label, active, ...rest } = props;
+  return (
+    <label className={`flex items-center px-4 py-2 border rounded-lg cursor-pointer ${active ? 'bg-green-100 text-green-800' : 'bg-white text-gray-700'}`}>
+      <input
+        type="radio"
+        checked={active}
+        className="hidden"
+        {...rest}
+      />
+      <span>{label}</span>
+    </label>
+  )
+}
+
+
+export const SelectField: React.FC<SelectFieldProps> = (props) => {
+  const { options, selectedValue, error, label, ...rest } = props;
+  const selected = options.find((option) => String(option.value) === selectedValue);
+  return (
+    <div className="space-y-2">
+      <label htmlFor={rest.name} className="block text-sm font-medium text-gray-700">
+        {label}
+      </label>
+      <select
+        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        {...rest}
+        value={selected?.value ?? ''}
+      >
+        <option value="">Select a category</option>
+        {options.map(option => (
+          <option key={option.value} value={option.value}>{option.label}</option>
+        ))}
+      </select>
+      {error && (
+        <Alert variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
+    </div>
+  )
+}
---
+++ src/views/posts/update-post.tsx
@@ -0,0 +1,144 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom"
+import { Card, CardContent, CardHeader, CardTitle } from "../../components/ui/card"
+import { useUpdatePostMutation } from "../../app/api-upload";
+import { NewPostErrors, UpdatePostRequest } from "../../types/post-types";
+import { processRequestError } from "../../lib";
+import { useToast } from "../../components/ui/toast/toast-context";
+import { RadioInput, /*SelectField,*/ TextAreaInput, TextInput } from "../../components/ui/input";
+import { useGetAllCategories } from "../../hooks/category-hook";
+
+export default function UpdatePost() {
+  const { state } = useLocation();
+  const toast = useToast();
+  const navigate = useNavigate();
+  const [updateHandler, { isLoading }] = useUpdatePostMutation();
+  const { data: categories } = useGetAllCategories();
+
+  const [formValue, setFormValue] = useState<UpdatePostRequest>(state?.post as UpdatePostRequest);
+  const [errors, setErrors] = useState<NewPostErrors>(Object);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormValue((prev: UpdatePostRequest) => ({
+      ...prev,
+      [name]: value
+    }));
+    if (errors[name]) {
+      setErrors((prev: NewPostErrors) => ({ ...prev, [name]: undefined }));
+    }
+  };
+
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { author_id, category, content, id, status, title, excerpt, tags } = formValue;
+    // TODO: Fix updating categories
+    const categoryId = categories.find((option) => option.label === category as unknown);
+    updateHandler({
+      author_id,
+      category: categoryId?.value as number,
+      content,
+      id,
+      status,
+      title,
+      excerpt,
+      tags
+    }).unwrap().then(() => {
+      toast.open({
+        message: 'Successful',
+        variant: "success",
+      });
+      navigate('/dashboard')
+    }).catch((e) => {
+      toast.open({
+        message: processRequestError(e, 'Failed to update post. Please try again later.'),
+        variant: "destructive",
+      });
+    });
+  }
+  return (
+    <div className="max-w-4xl mx-auto p-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Update Post</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <TextInput
+              label='Title *'
+              error={errors.title}
+              placeholder="Enter your blog post title"
+              id="title"
+              name="title"
+              value={formValue.title}
+              onChange={handleInputChange}
+            />
+
+            <TextInput
+              label='Excerpt'
+              placeholder="Enter an excerpt (optional)"
+              id="excerpt"
+              name="excerpt"
+              value={formValue.excerpt}
+              onChange={handleInputChange}
+            />
+
+            {/* <SelectField
+              id="category"
+              name="category"
+              selectedValue={formValue.category}
+              onChange={handleInputChange}
+              label='Category *'
+              error={errors.category}
+              options={categories}
+            /> */}
+
+            <TextAreaInput
+              error={errors.content}
+              label='Content *'
+              id="content"
+              name="content"
+              value={formValue.content}
+              onChange={handleInputChange}
+              placeholder="Write your blog post content here..."
+            />
+
+            <div className="mb-4">
+              <label className="block text-gray-700">Status</label>
+              <div className="flex items-center space-x-4">
+                <RadioInput
+                  type="radio"
+                  name="status"
+                  value="Draft"
+                  onChange={() => setFormValue(prev => ({ ...prev, status: 'Draft' }))}
+                  active={formValue.status === 'Draft'}
+                  label='Draft'
+                />
+
+                <RadioInput
+                  type="radio"
+                  name="status"
+                  value="Draft"
+                  onChange={() => setFormValue(prev => ({ ...prev, status: 'Published' }))}
+                  active={formValue.status === 'Published'}
+                  label='Published'
+                />
+              </div>
+            </div>
+
+            <div className="flex justify-end space-x-4">
+              <button
+                type="submit"
+                disabled={isLoading}
+                className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+              >
+                Update
+              </button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
